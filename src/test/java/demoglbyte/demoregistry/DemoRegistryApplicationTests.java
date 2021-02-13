e3890fe2ee294329d7c196be44d1fb771c20cf6b
Kiran Thota <kiranthota17@gmail.com>
2021-02-13 23:26:51 +0530

    Initial Commit

---
+++ src/test/java/demoglbyte/demoregistry/DemoRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package demoglbyte.demoregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
